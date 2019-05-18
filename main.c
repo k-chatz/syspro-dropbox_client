63dd169f390a2460a5bcaa1d874d569b49b3aa9a
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-05-18 22:59:24 +0300

    command line arguments.

--- main.c
+++ main.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+void wrongOptionValue(char *opt, char *val) {
+    fprintf(stderr, "\nWrong value [%s] for option '%s'\n", val, opt);
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * Read options from command line*/
+void readOptions(
+        int argc,
+        char **argv,
+        char **dirname,                     /*dirname*/
+        int *portNum,                       /*portNum*/
+        int *workerThreads,                 /*workerThreads*/
+        int *bufferSize,                    /*bufferSize*/
+        int *serverPort,                    /*serverPort*/
+        char **serverIP                     /*serverIP*/
+) {
+    int i;
+    char *opt, *optVal;
+    for (i = 1; i < argc; ++i) {
+        opt = argv[i];
+        optVal = argv[i + 1];
+        if (strcmp(opt, "-d") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *dirname = optVal;
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-p") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *portNum = (int) strtol(optVal, NULL, 10);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-w") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *workerThreads = (int) strtol(optVal, NULL, 10);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-b") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *bufferSize = (int) strtol(optVal, NULL, 10);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-sp") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *serverPort = (int) strtol(optVal, NULL, 10);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-sip") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *serverIP = optVal;
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *dirname = NULL, *serverIP = NULL;
+    int portNum = 0, workerThreads = 0, bufferSize = 0, serverPort = 0;
+
+    /* Read argument options from command line*/
+    readOptions(argc, argv, &dirname, &portNum, &workerThreads, &bufferSize, &serverPort, &serverIP);
+
     printf("Hello, World!\n");
     return 0;
 }
